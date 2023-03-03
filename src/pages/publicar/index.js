ff22156cd7e36d867d8ab78d6d13415383334012
Ana <ana_leticia@geb.inatel.br>
2023-03-02 22:33:37 -0300

    implementação spa.
    Co-authored-by: ajuliamm <ajuliammelo@gmail.com>
    Co-authored-by: patriciadania <patricia.adania@hotmail.com>

--- src/pages/publicar/index.js
+++ src/pages/publicar/index.js
@@ -1,13 +1,15 @@
-// aqui exportaras las funciones que necesites
+  import Header from "../../components/header/index.js"
+  import Footer from "../../components/footer/index.js"
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
-};
-
-export default() => {
+  export default() => {
   const container = document.createElement('div');
+
+  container.append(Header());
+
+  const sectionMain = document.createElement('section');
+
   const template = ` 
+  <main class="display" >
   <div class="feed display">
     <p class="username">Username</p>
     <textarea class="textarea" placeholder="Digite aqui a sua mensagem"></textarea>
@@ -15,11 +17,16 @@ export default() => {
       <button type="button">Postar</button>
     </div>
   </div>   
+  </main>
   `;
+    
+  sectionMain.innerHTML = template;
 
-  container.innerHTML = template;
+  container.append(sectionMain);
+  container.append(Footer());
+  
   return container;
-}
+  }
 
 
  
