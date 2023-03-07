7682debaac04b83bc41e92739125d7cb6ee9d609
bruna-abreu <bruna.abreu95@gmail.com>
2023-03-07 14:07:30 -0300

    estrutura-login

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+//import { myFunction } from './lib/index.js';
 
-myFunction();
+//myFunction();
+
+import login from './login/login.js';
+
+const main = document.querySelector('#root');
+
+window.addEventListener('load', () => {
+  main.appendChild(login());
+});
