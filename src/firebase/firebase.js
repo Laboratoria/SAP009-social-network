b2b9f820aff31399ffa57bda3a7ad86c1c992c69
paola-oliveira <paolanatalia_oliveira@outlook.com>
2023-03-09 19:10:38 -0300

    ajustes nas identações e inicio dos testes unitários. > > Co-authored-by: paulajanu <annapsjanuario@gmail.com> Co-authored-by: ThainaraTabile <thainara.tabilebenevides@gmail.com>

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,40 +1,48 @@
-import firebaseConfig from './firebase-config.js';
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  sendPasswordResetEmail,
+} from 'firebase/auth';
+import { firebaseConfig } from './firebase-config';
 // os imports acima são de funções do firebase
-import { redirecionarPagina } from "../redirecionar-pagina";
-
+import { redirecionarPagina } from '../redirecionar-pagina';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider(app);
 
-//const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // aqui vamos criar as const que autenticar, registrar...
 
-function autenticarUsuario(email,senha) {
-    return signInWithEmailAndPassword(auth, email, senha);
+function autenticarUsuario(email, senha) {
+  return signInWithEmailAndPassword(auth, email, senha);
 }
 
-//registrar novo usuário
+// registrar novo usuário
 function criarUsuario(email, senha) {
-    return createUserWithEmailAndPassword(auth, email, senha);
-  }
-
+  return createUserWithEmailAndPassword(auth, email, senha);
+}
 
 // login com google
 function logarGoogle() {
-    signInWithPopup(auth, provider)
-        .then(() => {
-            redirecionarPagina('#feed');
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+  signInWithPopup(auth, provider)
+    .then(() => {
+      redirecionarPagina('#feed');
+    })
+    .catch(() => {
+    });
 }
 
 const redefinirSenha = (email) => sendPasswordResetEmail(auth, email);
 
-export {autenticarUsuario, criarUsuario, logarGoogle, redefinirSenha};
-
+export {
+  autenticarUsuario,
+  criarUsuario,
+  logarGoogle,
+  redefinirSenha,
+};
