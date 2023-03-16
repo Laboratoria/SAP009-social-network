7180687d8a92c61bf47b6dbf973b8a811a13e824
Aline Guiseline <alineguiseline@gmail.com>
2023-03-16 16:37:08 -0300

    reformulação do projeto

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -11,11 +11,11 @@ export default () => {
   container.innerHTML = template;
 
   const logoutButton = container.querySelector('#logout-button');
- logoutButton.addEventListener('click', () => {
+  logoutButton.addEventListener('click', () => {
     LogOut();
     // Adicione aqui o código que deve ser executado quando o botão de login é clicado
 
     window.location.replace('#login');
   });
-  return container; 
+  return container;
 };
