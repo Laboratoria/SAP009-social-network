a22008e9cbc35a0ddeed30461fe228f0102e671f
ajuliamm <ajuliammelo@gmail.com>
2023-03-17 14:54:26 -0300

    username do usuário, datas e testes.
    Co-authored-by: analeticiabacha <ana_leticia@geb.inatel.br>
    Co-authored-by: patriciadania <patricia.adania@hotmail.com>

--- src/servicesFirebase/firebaseStore.js
+++ src/servicesFirebase/firebaseStore.js
@@ -1,40 +1,18 @@
 /* eslint-disable no-console */
 import { collection, addDoc, getDocs } from 'firebase/firestore';
 import { db } from '../firebaseInit';
-import { Auth } from './firebaseAuth';
 
-export const userData = async (name, lastname) => {
-  try {
-    const docRef = await addDoc(collection(db, 'infos-add'), {
-      nome: name,
-      sobrenome: lastname,
-    });
+export const userData = (name, lastname) => addDoc(collection(db, 'infos-add'), {
+  nome: name,
+  sobrenome: lastname,
+});
 
-    // eslint-disable-next-line no-console
-    console.log('Document written with ID: ', docRef.id);
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error('Error adding document: ', e);
-  }
-};
-
-export const newPost = async (postagem) => { // colocar data da postagem
-  try {
-    const timeElapsed = Date.now();
-    const today = new Date(timeElapsed);
-    const dataPostagem = today.toLocaleDateString();
-    console.log(Auth.currentUser.uid);
-    const docRef = await addDoc(collection(db, 'posts'), {
-      userName: Auth.currentUser.displayName,
-      data: dataPostagem,
-      post: postagem,
-      idUser: Auth.currentUser.uid,
-    });
-    console.log('Document written with ID: ', docRef.id);
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
-};
+export const newPost = (postagem, dataPostagem, username) => addDoc(collection(db, 'posts'), {
+  userName: username,
+  data: dataPostagem,
+  post: postagem,
+  // idUser: Auth.currentUser.uid,
+});
 
 export const accessPost = async () => {
   const querySnapshot = await getDocs(collection(db, 'posts'));
@@ -45,8 +23,3 @@ export const accessPost = async () => {
   });
   return messages;
 };
-
-// export const datePost = async
-//   const timeElapsed = Date.now();
-//   const today = new Date(timeElapsed);
-//   const dataPostagem = today.toLocaleDateString();
---
+++ test/servicesFirebase/firebaseStore.spec.js
@@ -0,0 +1,66 @@
+import query from 'esquery';
+import { addDoc, collection, getDocs, QuerySnapshot } from 'firebase/firestore';
+import { newPost, userData, accessPost } from '../../src/servicesFirebase/firebaseStore';
+// import { db } from "../../src/firebaseInit";
+// import { Auth } from "../../src/servicesFirebase/firebaseAuth";
+
+jest.mock('firebase/firestore');
+
+// tirar consoles e catchs
+
+it('deve acessar os dados do usuário e guardar na coleção', async () => {
+  // userData
+  addDoc.mockResolvedValueOnce();
+  const mockCollection = 'collection';
+  collection.mockReturnValueOnce(mockCollection);
+  const name = 'nome';
+  const lastname = 'sobrenome';
+  const infosAdd = {
+    nome: name,
+    sobrenome: lastname,
+  };
+  await userData(name, lastname);
+
+  expect(addDoc).toHaveBeenCalledTimes(1);
+  expect(addDoc).toHaveBeenCalledWith(mockCollection, infosAdd);
+  expect(collection).toHaveBeenCalledTimes(1);
+  expect(collection).toHaveBeenCalledWith(undefined, 'infos-add');
+});
+
+it('deve criar um post e guardar na coleção', async () => {
+  // newPost
+  addDoc.mockResolvedValueOnce();
+  const mockCollection = 'collection';
+  collection.mockReturnValueOnce(mockCollection);
+  const dataPostagem = 'xx/xx/xxxx';
+  const postagem = 'texto';
+  const username = 'usernameTeste';
+  const posts = {
+    userName: username,
+    data: dataPostagem,
+    post: postagem,
+    // idUser: 'wxyzUser',
+  };
+  await newPost(postagem, dataPostagem, username);
+
+  expect(addDoc).toHaveBeenCalledTimes(1);
+  expect(addDoc).toHaveBeenCalledWith(mockCollection, posts);
+  expect(collection).toHaveBeenCalledTimes(1);
+  expect(collection).toHaveBeenCalledWith(undefined, 'posts');
+});
+
+it('deve acessar a publicação criada', async () => {
+  // accessPost
+  getDocs.mockResolvedValueOnce();
+  const mockCollection = ['1', '2', '3'];
+  collection.mockReturnValueOnce(mockCollection);
+  const querySnapshot = mockCollection;
+  // const data;
+  // const messages = [];
+  await accessPost();
+
+  expect(getDocs).toHaveBeenCalledTimes(1);
+  expect(getDocs).toHaveBeenCalledWith(mockCollection);
+  expect(collection).toHaveBeenCalledTimes(1);
+  expect(collection).toHaveBeenCalledWith(undefined, 'posts');
+});
