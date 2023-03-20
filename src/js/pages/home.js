f92cafb4300e7f3ac6f1f8ba0af6c3ebff2eaf6d
marinacezario <marina.cezario@gmail.com>
2023-03-20 19:38:31 -0300

    css inicio

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -3,31 +3,32 @@ export default () => {
 
   const homeTemplate = `
 <section class="home-main">
-    <img id="logo-complete" src="img/logo-s2s-transparent.png">
-    <div class="home-initial-text">  
-        <h1>The best social media for learning English!</h1>
-        <br>
-        <h2>A melhor rede social para aprender Inglês!</h2>
-        <br>
+  <img id="logo-complete" src="img/logo-s2s-transparent.png">
+  <div class="home-initial-text">  
+    <h1>The best social media for learning English!</h1>
+    <br>
+    <h2>A melhor rede social para aprender Inglês!</h2>
+    <br>
+  </div>
+  <div class="main-content-home>
+    <div class="icons-text-home">
+      <img src="./img/location-dot-solid.svg" alt="Location dot icon" class"icons-home">
+      <p class="text-home">Estudantes de todo o país se encontram aqui.</p>
     </div>
-    <div class="icons-home">
-        <img src="./img/location-dot-solid.svg" alt="Location dot icon" class"icons-blue">
-        <img src="./img/logo-icon-no-background.png" alt="Logo s2s three people icon" class"icons-orange">
-        <img src="./img/pen-to-square-regular.svg" alt="Pen to square icon" class"icons-blue">
-        <img src="./img/heart-solid.svg" alt="Love icon"  class"icons-orange">
+    <div class="icons-text-home">
+      <img src="./img/logo-icon-no-background.png" alt="Logo s2s three people icon" class"icons-home">
+      <p class="text-home">Participe de comunidades sobre músicas, filmes, séries, memes, gramática e muito mais.</p>
     </div>
-    <div class="text-home">
-        <p>Estudantes de todo o país se encontram aqui.</p>
-        <br>
-        <p>Participe de comunidades sobre músicas, filmes, séries, memes,
-        gramática e muito mais.</p>
-        <br>
-        <p>Crie postagens e compartilhe ideias e pensamentos.</p>
-        <br>
-        <p>Curta conteúdos de outras pessoas e faça amizades.</p>
-        <br>
+    <div class="icons-text-home">
+      <img src="./img/pen-to-square-regular.svg" alt="Pen to square icon" class"icons-home">
+      <p class="text-home">Crie postagens e compartilhe ideias e pensamentos.</p>
     </div>
-    <button id="sign-in-home">SIGN IN</button>
+    <div class="icons-text-home">
+      <img src="./img/heart-solid.svg" alt="Love icon"  class"icons-home">
+      <p class="text-home">Curta conteúdos de outras pessoas e faça amizades.</p>
+    </div>
+  </div>
+  <button id="sign-in-home-button">SIGN IN</button>
 </section>
   `;
     /* inserir container header */
