c37cd871713a9fbced15e45b248e086528484c5c
osikmelina <melina.mk@gmail.com>
2023-03-23 18:24:08 -0300

    funções de login e criação de página de cadastro

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,9 +1,9 @@
-import { fazerLogin } from '../../auth';
+import { fazerLogin, loginGoogle} from '../../firebase/auth.js';
+
 export default () => {
   const container = document.createElement('div');
-
   const template = ` 
-    <div class="conteudo-login">
+    <section class="conteudo-login">
     <div class = "figura-menina-texto">
       <p class="descricao">Faça parte da Le<span style= "color:#FF7373;">m</span>os , a maior rede social de incentivo e compartilhamento de leitura feminista! </p>
       <img class="menina-lemos" src="imagens/lemos-figura.png">
@@ -13,31 +13,48 @@ export default () => {
         <input type="text" class="email-login" id="email-login" placeholder="E-MAIL" required><br>
         <input type="password" class="senha-login" id="senha-login" placeholder="SENHA" required><br>
         <p class="texto-senha"> Esqueci a senha </p>
+        <p class="texto-erro" id="textoErro"></p>
         <button type="submit" class="botao-entrar">Entrar</button>
         <p class="texto-ou"> ou </p>
         <img class="botao-google" src="imagens/figura-google.png">
-        <p class="texto-cadastro">Ainda não faz parte? <br> Cadastre-se aqui!</p>
+        <p class="texto-cadastro">Ainda não faz parte? <br> <a href="#cadastro">Cadastre-se aqui!</p></a>
     </form>
     
-    </div>
+    </section>
     `;
 
   container.innerHTML = template;
+
   const loginEmail = container.querySelector('#email-login');
-  console.log(loginEmail);
-
   const loginSenha = container.querySelector('#senha-login');
-  console.log(loginSenha);
-
   const botaoEntrar = container.querySelector('.botao-entrar');
-  console.log(botaoEntrar);
+  const botaoGoogle = container.querySelector('.botao-google');
+  const mensagemErro = container.querySelector('.texto-erro');
 
-  botaoEntrar.addEventListener('click', () => {
-    const email = loginEmail.value;
-    const senha = loginSenha.value;
-    fazerLogin(email, senha);
-    console.log(email, senha);
+  botaoEntrar.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    fazerLogin(loginEmail.value, loginSenha.value)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+
+        window.location.hash = '#feed';
+      })
+      .catch(() => {
+        mensagemErro.innerHTML = 'Usuário ou senha incorretos';
+      });
   });
+
+  botaoGoogle.addEventListener('click', () => {
+    loginGoogle()
+      .then(() => {
+        window.location.hash = '#feed';
+      })
+      .catch(() => {
+        mensagemErro.innerHTML = 'Erro ao logar com Google';
+      });
+});
 
   return container;
 };
