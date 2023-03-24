d35b21ffc94bcd79c1a2618559095b07d83f1927
Aline Guiseline <alineguiseline@gmail.com>
2023-03-24 16:38:11 -0300

    Alteração nos códigos de criação e exibição de posts

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,16 +1,18 @@
 import {
-  getFirestore, addDoc, collection, getDocs, query, orderBy,
+  getFirestore, addDoc, collection, getDocs, orderBy,
 } from 'firebase/firestore';
 import { app } from './app';
 
 const db = getFirestore(app);
 
-export const createPost = async (anime, episodes, description) => addDoc(collection(db, 'posts'), {
-  animeName: anime,
-  episodesAnime: episodes,
-  descriptionAnime: description,
-});
-
+export const createPost = (anime, episodes, description) => { //eslint-disable-line
+  return addDoc(collection(db, 'posts'), {
+    anime,
+    episodes,
+    description,
+  });
+};
+/*
 export const accessPost = async () => {
   const posts = [];
   const queryOrder = query(collection(db, 'posts'), orderBy('date', 'desc'));
@@ -23,3 +25,34 @@ export const accessPost = async () => {
   });
   return posts;
 };
+*/
+
+export const accessPost = async () => {
+  const querySnapshot = await getDocs(collection(db, 'posts'), orderBy('date', 'desc'));
+  const posts = [];
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    data.id = doc.id;
+    posts.push(data);
+    // post.push({ ...post.data(), id: post.id });
+  });
+  return posts;
+};
+
+/*
+export const accessPost = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'posts'), orderBy('date'));
+    const posts = [];
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      data.id = doc.id;
+      posts.push(data);
+      // post.push({ ...post.data(), id: post.id });
+    });
+    return posts;
+  } catch (error) {
+    return error;
+  }
+};
+*/
