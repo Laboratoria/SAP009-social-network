7b5df141c8c60004e53ed88ea2c6bc0b26d8e763
marianalopesdev <marianalopes.dev@gmail.com>
2023-03-24 00:33:34 -0300

    fn createPost modified to use a new collection outside users collection

--- src/firestore/DBFunctions.js
+++ src/firestore/DBFunctions.js
@@ -7,27 +7,33 @@ import { auth } from '../firebase/auth';
 export const createNewPost = async (textPost) => {
   const post = {
     uid: auth.currentUser.uid,
-    name: auth.currentUser.displayName,
-    title: textPost,
-    message: 'fabicon',
-
+    displayName: auth.currentUser.displayName,
+    title: 'vendo guitarra',
+    textPost: 'vendo guitarra azul, bom estado',
+    dateTime: '2023-03-24 00:08',
+    updateDateTime: '',
+    likes: [1],
   };
   // grava o post com o UID na collection posts independente de users
-  // const docReference = await addDoc(collection(db, 'posts'), post);
-  const docReference = await addDoc(collection(db, 'users', auth.currentUser.uid, 'posts'), post);
+  const docReference = await addDoc(collection(db, 'posts'), post);
+  // const docReference = await addDoc(collection(db, 'users', auth.currentUser.uid, 'posts'), post);
   // post.id = teste.id;
   post.id = docReference.id;
   return post;
 };
 
 export const getLoggedUserAllPosts = async () => {
-  const postsCollection = await getDocs(collection(db, 'users', auth.currentUser.uid, 'posts'));
+  const postsCollection = await getDocs(collection(db, 'posts'));
+  // grava na collection posts dentro da collection users
+  // await getDocs(collection(db, 'posts', auth.currentUser.uid, 'posts'));
+  // console.log(postsCollection);
   const posts = [];
   postsCollection.forEach((post) => {
     const data = post.data();
-    data.id = post.id;
-    posts.push(data);
+    if (data.uid === auth.currentUser.uid) {
+      data.id = post.id;
+      posts.push(data);
+    }
   });
-  console.log(posts);
   return posts;
 };
