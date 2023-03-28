95bd1ea42a2ce52da29f43e9844da0d99ca56bbb
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-03-28 14:08:41 -0300

    pagina de login ok(mobile first)

--- src/js/components/headers/header-login.js
+++ src/js/components/headers/header-login.js
@@ -3,19 +3,19 @@ export default () => {
   headerLoginContainer.classList.add('header-login');
 
   const headerLoginTemplate = `
-     <header>
-        <div class="header-nav">
-          <ul>
-            <li id="header-link">
-              <a href="/#home"><img src="./img/logo-icon-no-background.png" alt="logo-icon" id="logo-icon"></a>
-            </li>
-            <li id="li-header-btn">
-              <button href="/#sign-up" id="header-btn">SIGN UP</button>
-            </li>
-          </ul>
-        </div>
-      </header>
-      `;
+  <div class="header-nav">
+    <ul>
+      <li id="header-link">
+        <a href="/#home"><img src="./img/logo-icon-no-background.png" alt="logo-icon" id="logo-icon"></a>
+      </li>
+      <li id="li-header-btn">
+        <button id="header-btn">
+          <a href="/#signup">SIGN UP</a>
+        </button>
+      </li>
+    </ul>
+  </div>
+   `;
 
   headerLoginContainer.innerHTML = headerLoginTemplate;
 
