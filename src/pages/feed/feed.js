b1f73f28771ab47f00f2c57c294f0f1d97043d3a
talita <talitamsx@gmail.com>
2023-03-28 08:52:48 -0300

    -

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -4,8 +4,37 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-  <h2>aqui vão as postagens</h2>
-  <button type="button" class="botao-sair">Sair</button>
+  <header class="conteudo-feed">
+    <div class="cabecalho-feed">
+      <img class="logo-feed" src="/imagens/logo-lemos.png" href="/#login">
+      <button type="button" class="botao-sair">Sair</button>
+    </div>
+  </header>
+  <main class="feed-post">
+    <section class="nome-livro">
+      <h2 class="criar-post">Criar postagem</h2>
+      <p>Título do livro</p>
+      <input type="text" class="input-titulo">
+    </section>
+    <section class="nome-autora">
+      <p>Autora</p>
+      <input type="text" class="input-autora">
+    </section>
+    <section class="nivel-leitura">
+      <p>Para qual leitora você indica esse livro?</p>
+      <div class="botoes-nivel">
+        <button class="iniciante">iniciante</button>
+        <button class="intermediaria">intermediária</button>
+        <button class="avancada">avançada</button>
+      </div>
+    </section>
+    <section class="post-publicacao">
+      <textarea placeholder="Limite de 350 caracteres..."></textarea>
+    </section>
+    <button type="submit" class="botao-publicar">Publicar</button>
+    <h2 class="ultimos-posts">Últimas publicações</h2>
+  </main>
+
   `;
 
   container.innerHTML = template;
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,8 +1,15 @@
-import { fazerLogin, loginGoogle} from '../../firebase/auth.js';
+import { fazerLogin, loginGoogle } from '../../firebase/auth.js';
 
 export default () => {
   const container = document.createElement('div');
   const template = ` 
+
+  <header class="conteudo">
+    <div class="menu-superior">
+      <img class="img-logo" src="/imagens/logo-lemos.png" href="/#login">
+     <h2 class="subtitulo">Incentivando a leitura feminista</h2> 
+    </div>
+  </header>
     <section class="conteudo-login">
     <div class = "figura-menina-texto">
       <p class="descricao">Faça parte da Le<span style= "color:#FF7373;">m</span>os , a maior rede social de incentivo e compartilhamento de leitura feminista! </p>
@@ -16,7 +23,7 @@ export default () => {
         <p class="texto-erro" id="textoErro"></p>
         <button type="submit" class="botao-entrar">Entrar</button>
         <p class="texto-ou"> ou </p>
-        <img class="botao-google" src="imagens/figura-google.png">
+        <img class="botao-google" a href="#feed" src="imagens/figura-google.png"></a>
         <p class="texto-cadastro">Ainda não faz parte? <br> <a href="#cadastro">Cadastre-se aqui!</p></a>
     </form>
     
@@ -54,7 +61,6 @@ export default () => {
       .catch(() => {
         mensagemErro.innerHTML = 'Erro ao logar com Google';
       });
-});
-
+  });
   return container;
 };
