48cdb035b9ab32dedf5e75b9bea75f82fe7cb7a0
Keila <keilaoliveira.copy@gmail.comm>
2023-03-28 17:13:55 -0300

    correção do codigo de login

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,22 +1,13 @@
-import { initializeApp } from 'firebase/app';
+/* import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD6gfUeqRQFZaJQ2UDvkziJ7sYU-1oTFVc',
-  authDomain: 'brulle-2c8c1.firebaseapp.com',
-  projectId: 'brulle-2c8c1',
-  storageBucket: 'brulle-2c8c1.appspot.com',
-  messagingSenderId: '1036171396591',
-  appId: '1:1036171396591:web:9d114e594e3fcfea1b221a',
-  measurementId: 'G-DTYHMXZSBC',
-};
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
 
-export {
+
+/* const provider = new GoogleAuthProvider(); */
+/* export {
   auth, app, analytics,
 
 };
+ */
--- src/servicesFirebase/firebaseAuth.js
+++ src/servicesFirebase/firebaseAuth.js
@@ -1,30 +1,61 @@
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebaseInit';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+/*  GoogleAuthProvider,
+signInWithPopup
+*/ 
+updateProfile, } from 'firebase/auth';
+import { firebaseConfig } from './firebaseconfig';
 
-/*Cadastrar usuários*/
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+/* Cadastrar usuários */
 export const createUser = (email, senha) => {
   createUserWithEmailAndPassword(auth, email, senha)
     .then((userCredential) => {
       const user = userCredential.user;
       console.log('foi');
       window.location.hash = '#login';
-    })
-    .catch((error) => {
+    });
+   /*  .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-    });
+    }); */
+  return updateProfile(user, { email, senha });
 };
 
 export const valuesLogin = (email, senha) => {
   signInWithEmailAndPassword(auth, email, senha)
     .then((userCredential) => {
-      window.location.href = "pages/feed/index.html";
-      console.log("foi");
+      window.location.href = '#feed';
+      console.log('foi');
       const user = userCredential.user;
     })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage);
+    .catch(() => {
+     /*  txtError.setAttribute('style', 'display: block');
+      txtError.innerHTML = 'Usuário ou senha incorretos' */;
+      /* senha.focus(); */
     });
-};
+}; 
+/* signInWithPopup(auth, provider);
+    .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+ */
