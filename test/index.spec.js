94d6202f094304f67396645b8fd9f048a962d1b8
Keila <keilaoliveira.copy@gmail.comm>
2023-03-28 21:50:02 -0300

    teste da função de criar cadastro

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,25 @@
-import { createUser, valueLogin } from '../servicesFirebase/firebaseAuth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  getAuth,
+} from 'firebase/auth';
+
+import {
+  createUser,
+  valuesLogin,
+} from '../src/servicesFirebase/firebaseAuth';
+
+// função de criar usuário//
+
+describe('criarUsuario', () => {
+  it('a função deve criar uma conta do usuário utilizando o email e senha', async () => {
+    createUserWithEmailAndPassword.mockResolvedValueOnce();
+    const email = 'teste@teste.com';
+    const senha = '12345678';
+    await createUser(email, senha);
+
+    expect(createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
+    expect(createUserWithEmailAndPassword).toHaveBeenCalledWith(getAuth(), email, senha);
+  });
+});
