539d1fb22192614bf903aa8dade50c9fa3026c10
marinacezario <marina.cezario@gmail.com>
2023-03-29 13:14:27 -0300

    atualizações firebase

--- src/firebase/authentication.js
+++ src/firebase/authentication.js
@@ -1,3 +1,6 @@
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyBcJ5XDTRiMrUBYrEx2qgiH1B-igMqby0c',
   authDomain: 'student-2-student-93fb5.firebaseapp.com',
