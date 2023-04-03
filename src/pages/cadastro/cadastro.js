13412c08197e486c196ba7418b0a4bfbbd332782
Edimara Arcanjo <edimaraarcanjo21@gmail.com>
2023-04-03 14:52:46 -0300

    configurando css geral

--- src/pages/cadastro/cadastro.js
+++ src/pages/cadastro/cadastro.js
@@ -1,15 +1,8 @@
 export default () => {
-  const container = document.createElement('div');
-  container.classList.add('body-cadastro');
-  const templaite = `
-    
-    <h1 class='cadastro'>cadastro</h1>
-    <button class='btn-cadastro'>cadastrar</button>
-  
   const registerContainer = document.createElement('div');
   registerContainer.classList.add('body-cadastro');
   const template = `
-    <h1 class='title'>HelParents</h1>
+    <h1 class="logo-geral">HelParents</h1>
     <p><strong>Primeira vez por aqui?</strong></p>
     <p>Realize o seu cadastro, contribua e aproveite todas as possibilidades
     que esta rede pode oferecer!</p>
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,51 +1,34 @@
 export default () => {
   const container = document.createElement('div');
-  const templaite = `
-  
+  const template = `
   <header class='header-login-page'>
-  <p1>HelParents</p1>
-</header>
-
-
- <p1>A HelParents é uma rede social para você que é cuidador ou 
+    <h1 class="logo-geral">HelParents</h1>
+  </header>
+  <div class="paragrafo">
+    <p>A HelParents é uma rede social para você que é cuidador ou 
   responsável por alguma criança e/ou adolescente e busca 
   um espaço seguro para o compartilhamento de informações 
-  e orientações  sobre o uso da internet e seus aplicativos por esse público. </p1>
+  e orientações  sobre o uso da internet e seus aplicativos por esse público.</p>
+    <p><strong>Já está cadastrado? Faça o login!</strong></p>
+  </div>  
+  <form class='section-login'>
+      <h2>LOGIN</h2>
+      <input type='text' placeholder='Usuário' id='name'>
+      <input type='password' placeholder='Senha' id='password'> 
+      <div class="form-group">
+            <button type="submit" class="btn-feed">LOGIN</button>
+                <p>Acesse com o Google<p>
+            <button type="submit" class="btn-login">GOOGLE</button>
+                <p> Ainda não tem conta?<p>
+                <button type="submit" class="btn-cadastro btn-geral">CADASTRE-SE</button>
+        </div>
+    </form> 
+  `;
 
-            <p1>Já está cadastrado ? Faça o login!</p1>
-
-        <form class='section-login-dispay'>
-
-        <br>
-
-    <h1> LOGIN </h1>
-    <div class="row align-items-center h-100 ">
-    <div class="col-8 col-md-3 col-xs-8 mx-auto l-form">
-        <form class="form" method="post">
-            <div class="form-group ">
-                <input type="text" placeholder="Usuário" class="form-control i-form">
-            </div>
-            <div class="form-group">
-                <input type="password" placeholder="Senha" class="form-control i-form">
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-dark btn-md btn-block">LOGIN</button>
-                <br>
-                <p1>Acesse com o Google<p1>
-                <br>
-                <button type="submit" class="btn btn-dark btn-md btn-block">GOOGLE</button>
-                <br>
-                <p1> Ainda não tem conta?<p1>
-                <br>
-                <button type="submit" class="btn btn-dark btn-md btn-block">CADASTRE-SE</button>
-                </div>
-        </form>
-    </div>
-</div>
-   
-    
-`;
-
-  container.innerHTML = templaite;
+  container.innerHTML = template;
+  const btnLogin = container.querySelector('.btn-feed');
+  btnLogin.addEventListener('click', () => {
+    window.location.hash = '#feed';
+  });
   return container;
 };
