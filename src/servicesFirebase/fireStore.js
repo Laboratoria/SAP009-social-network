c00a497935a3d00073d6bf9f263949d7b5780bec
Stellazen <stellazen.xd@gmail.com>
2023-04-04 17:02:38 -0300

    testando commit

--- src/servicesFirebase/fireStore.js
+++ src/servicesFirebase/fireStore.js
@@ -1,12 +1,28 @@
-/* import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+
 import {
   getFirestore,
 } from 'firebase/firestore';
 
 import {
   firebaseConfig,
+  collection,
+  addDoc,
 } from './firebaseconfig';
 
 const app = initializeApp(firebaseConfig);
-// FIRESTORE
-/* const db = getFirestore(app); */
+const db = getFirestore(app);
+
+//função newPost - postagem, nome do usuário, data
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
