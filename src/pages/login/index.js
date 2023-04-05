6e0f03d8bfb6409889e24cde109600abad8816e8
IzadoraNascimento <izasnascimento@icloud.com>
2023-04-05 18:10:23 -0300

    responsividade iniciando

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,52 +1,55 @@
-export default() =>{
-    const container = document.createElement('div');
-    container.classList.add('container-login');
+export default () => {
+  const container = document.createElement("div");
+  container.classList.add("container-login");
 
-    const template = `
-        <img class="icon-1" src="image/Rectangle 86.png">
+  const template = `
+    <img class="icon-1" src="image/Rectangle 86.png">
         <section class="bloco-login">
-            <header>
-                <h3>Bem-vindo à C&H</h3>
-                <label>Não tem conta?</label>
-                <a href="#cadastro" type="button" style="text-decoration:none">Cadastre-se</a>
-                <h1>Login</h1>
-            </header>
+            <div class="form-header">
+                <div class="title">
+                    <span>Bem Vindo a C&H</span>
+                    <h1>Não tem conta?</h1>
+                </div>
 
-            <section>
-                <a href="#">
-                <img class="logo-google" src="image/google.png" alt="imagem com logo do Google">
-                Entre com a sua conta Google
-                </a>
-            </section>
+                <div class="button-cadastro">
+                    <a href= "#cadastro" type="button" style="text-decoration:none">Cadastre-se</a>
+                </div>
+            </div>
+
+            <div class="google">
+                <button id="btn-gmail" type="button" class="btn-google">
+                    <img class="google-icon" src="image/google.png" alt="imagem com logo do Google">
+                    Continue com google
+                </button>
+            </div>
 
             <form class="form-login">
-                <div>
-                    <label>E-mail</label>
-                    <input type="text" name="username" id="username"
-                    placeholder="Digite seu e-mail">
-                </div>
+                <div class="input-group"
+                    <div class="input-box">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" id="email" placeholder="seu endereço de e-mail" required>
+                    </div>
 
-                <div>
-                    <label>Senha</label>
-                    <input type="password" name="password" id="password"
-                    placeholder="Digite sua senha">
-                </div>
+                    <div class="input-box">
+                        <label for="password">E-mail</label>
+                        <input type="password" name="password" id="password" placeholder="senha" required>
+                    </div>
 
-                <div>
-                    <label>Esqueci a senha</label>
-                </div>
+                    <div class="forgotpassword-container">
+                        <a href="#" class="forgotpassword">Esqueceu sua senha?</a>
+                    </div>   
 
-                <div>
-                    <button>
-                        <a>Entrar</a>
-                    </button>
-                </div>
+                    <div>
+                        <button>
+                            <a>Entrar</a>
+                        </button>
+                    </div>
             </form>
         </section>
-        <img class="icon-2" src="image/Rectangle 83.png">
+    <img class="icon-2" src="image/Rectangle 83.png">
     `;
 
-    container.innerHTML = template;
+  container.innerHTML = template;
 
-    return container;
-}
+  return container;
+};
