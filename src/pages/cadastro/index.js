18ea769efd3c8f22dad64f57ba9b060b73cbead8
IzadoraNascimento <izasnascimento@icloud.com>
2023-04-05 18:45:43 -0300

    atualizado

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,42 +1,51 @@
-export default ()=> {
-    const container=document.createElement('div');
-     container.classList.add('container-cadastro');
-    
-    const template=`
-     <img class = "icon-1" src="image/Rectangle 86.png" alt="imagem de menina lavando o rosto">
-     <section class = "bloco-cadastro"> 
-        <header> 
-        <h3> Bem vindo á C&H </h3> 
-        <h1> Cadastre-se </h1>
-        </header>
-    
-        <form class = "form-cadastro"> 
-            <div> 
-            <label> Email </label> 
-            <input type = "text" name= "username" id="username"
-            placeholder = "Digite seu email"> 
+export default () => {
+  const container = document.createElement("div");
+  container.classList.add("container-cadastro");
+
+  const template = `
+    <img class = "icon" src="image/Rectangle 86.png" alt="imagem de menina lavando o rosto">
+    <section class = "bloco-cadastro">
+        <div class="form-header">
+            <div class="title">
+                <span>Bem Vindo a C&H</span>
+                <h1>Cadastre-se</h1>
             </div>
-            <div> 
-            <label> Nome </label> 
-            <input type = "text" name= "username" id="name"
-            placeholder = "Seu nome"> 
-            </div><label> Número </label> 
-            <input type = "number" name= "username" id="number"
-            placeholder = "Seu número"> 
-            </div><label> Senha </label> 
-            <input type = "password" name= "username" id="password"
-            placeholder = "Senha"> 
+
+            <div class="return-button">
+                <a href= "#login" type="button" class="btnVoltar" style="text-decoration:none">Voltar</a>
             </div>
-            <div>
-            <button> Cadastrar </button>
+        </div>
+
+        <form class = "form-cadastro">
+            <div class="input-group">
+                <div class="input-box">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email" placeholder="seu endereço de e-mail" required>
+                </div>
+
+                <div class="input-box">
+                    <label for="name">Nome</label>
+                    <input type="text" name="name" id="txtName" placeholder="seu nome" required>
+                </div>
+
+                <div class="input-box">
+                    <label for="number">Número</label>
+                    <input type="tel" name="number" id="number" pattern="[0-9()-]+" placeholder="(xx) xxxx-xxxx" required>
+                </div>
+
+                <div class="input-box">
+                    <label for="password">Senha</label>
+                    <input type="password" name="password" id="password" placeholder="senha" required>
+                </div>
+            </div>
+
+            <div class="button-singnup">
+                <button id="btnSignup" type="button" class="btnSignup" style="text-decoration:none">Criar Conta</button>
             </div>
         </form>
-       
-     </section>
-     <img class = "icon-1" src="image/Rectangle 83.png" alt="imagem de menina mexendo no cabelo">
-     
-     `;
-     container.innerHTML = template;
-    return container;
-    
-    }
+    </section>
+    <img class="icon" src="image/Rectangle 83.png" alt="imagem de menina mexendo no cabelo">
+    `;
+  container.innerHTML = template;
+  return container;
+};
