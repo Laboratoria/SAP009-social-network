6c7e8071bf13b111aad97835f6701617891228fb
Edimara Arcanjo <edimaraarcanjo21@gmail.com>
2023-04-05 15:31:51 -0300

    firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXzj5MfGytnQSWwjsq5VKWlEfyc6Z6AMg',
+  authDomain: 'social-network-69ad3.firebaseapp.com',
+  projectId: 'social-network-69ad3',
+  storageBucket: 'social-network-69ad3.appspot.com',
+  messagingSenderId: '299175801388',
+  appId: '1:299175801388:web:21026ac0c0be9396261789',
+};
+
+export const app = initializeApp(firebaseConfig);
