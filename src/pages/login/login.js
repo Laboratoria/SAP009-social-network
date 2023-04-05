fd35faf383cc629bc8ee81da5100465779a5e36d
andraderika <oliandraderika@gmail.com>
2023-04-05 10:52:26 -0300

    resolvendo conflito

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,34 +1,44 @@
 export default () => {
   const container = document.createElement('div');
   const template = `
-  <header class='header-login-page'>
-    <h1 class="logo-geral">HelParents</h1>
-  </header>
-  <div class="paragrafo">
+    <section class='box-text-img'>
+    <div class='box-01'>
+    <h1 class="logo-geral logo-register">HelParents</h1>
+    <div class="paragrafo">
     <p>A HelParents é uma rede social para você que é cuidador ou 
-  responsável por alguma criança e/ou adolescente e busca 
-  um espaço seguro para o compartilhamento de informações 
-  e orientações  sobre o uso da internet e seus aplicativos por esse público.</p>
+    responsável por alguma criança e/ou adolescente e busca 
+    um espaço seguro para o compartilhamento de informações 
+    e orientações  sobre o uso da internet e seus aplicativos por esse público.</p>
     <p><strong>Já está cadastrado? Faça o login!</strong></p>
-  </div>  
-  <form class='section-login'>
-      <h2>LOGIN</h2>
-      <input type='text' placeholder='Usuário' id='name'>
-      <input type='password' placeholder='Senha' id='password'> 
+    </div> 
+    <div class='img-register'>
+    <img src='./img/img_cadastro.png' alt='img-cadastro' class='img-cadastro'>
+    </div>
+    </div>
+    <section class='box-register'>
+    <form class='section-register'>
+      <h2 class='subtitle-register'>LOGIN</h2>
+      <input type='text' placeholder='Usuário:' id='name'>
+      <input type='password' placeholder='Senha:' id='password'> 
       <div class="form-group">
-            <button type="submit" class="btn-feed">LOGIN</button>
-                <p>Acesse com o Google<p>
-            <button type="submit" class="btn-login">GOOGLE</button>
-                <p> Ainda não tem conta?<p>
-                <button type="submit" class="btn-cadastro btn-geral">CADASTRE-SE</button>
-        </div>
+      <button type="submit" class="btn-feed">LOGIN</button>
+      <p class='text-box-register'>Acesse com o Google<p>
+      <div class='logo-google'>
+      <img src='./img/googlelogo.png' alt='logo-google'>
+      </div>
+      <hr>
+      <p class='text-box-register'>Ainda não tem conta?<p>
+      <button type="submit" class="btn-cadastro">CADASTRE-SE</button>
+      </div>
     </form> 
-  `;
+    </section>
+    </section>  
+    `;
 
   container.innerHTML = template;
   const btnLogin = container.querySelector('.btn-feed');
   btnLogin.addEventListener('click', () => {
-    window.location.hash = '#feed';
+    window.location.hash = '#cadastro';
   });
   return container;
 };
