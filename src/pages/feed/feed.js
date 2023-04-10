f58d6ce45dd73c53d2de7e727921c9259c1f1bd3
talita <talitamsx@gmail.com>
2023-04-10 18:27:42 -0300

    teste

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -58,7 +58,7 @@ export default () => {
       <p class="dia-post"> ${post.date}</p>
     </div>  
       <section class="publicacao">
-        <p class="titulo-post">Título do Livro: ${post.titulo}</p>
+        <p class="titulo-post">Título do Livro: <strong>${post.titulo}</strong></p>
         <p class="autora-post">Nome da Autora: <strong>${post.autora}</strong></p>
         <p class="texto-postagem">${post.post}</p>
       </section> 
@@ -66,35 +66,35 @@ export default () => {
    `;
       containerPost.innerHTML = templatePost;
       localPost.appendChild(containerPost);
-
-      const titulo = container.querySelector('.input-titulo');
-      const autora = container.querySelector('.input-autora');
-      const postagem = container.querySelector('.texto-post');
-
-      function limparForm() {
-        container.querySelector('.input-titulo').value = '';
-        container.querySelector('.input-autora').value = '';
-        container.querySelector('.texto-post').value = '';
-      }
-
-      const botaoPublicar = container.querySelector('.botao-publicar');
-      botaoPublicar.addEventListener('click', async () => {
-        await fazerPost(titulo.value, autora.value, postagem.value);
-        limparForm();
-        console.log(titulo.value, autora.value, postagem.value);
-      });
-
-      const botaoSair = container.querySelector('.botao-sair');
-      botaoSair.addEventListener('click', () => {
-        fazerLogout()
-          .then(() => {
-            window.location.hash = '#login';
-          })
-          .catch(() => {
-          });
-      });
     });
   };
+
+  const titulo = container.querySelector('.input-titulo');
+  const autora = container.querySelector('.input-autora');
+  const postagem = container.querySelector('.texto-post');
+
+  function limparForm() {
+    container.querySelector('.input-titulo').value = '';
+    container.querySelector('.input-autora').value = '';
+    container.querySelector('.texto-post').value = '';
+  }
+  const botaoPublicar = container.querySelector('.botao-publicar');
+  botaoPublicar.addEventListener('click', async () => {
+    await fazerPost(titulo.value, autora.value, postagem.value);
+    limparForm();
+    console.log(titulo.value, autora.value, postagem.value);
+  });
+
+  const botaoSair = container.querySelector('.botao-sair');
+  botaoSair.addEventListener('click', () => {
+    fazerLogout()
+      .then(() => {
+        window.location.hash = '#login';
+      })
+      .catch(() => {
+      });
+  });
+
   exibirPostagem();
   return container;
 };
