6d99e8f9a3134e5b8ea5f97f5e923af85f6e05e7
IzadoraNascimento <izasnascimento@icloud.com>
2023-04-10 18:26:43 -0300

    iniciando o css da página

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -3,24 +3,24 @@ export default () => {
   container.classList.add("container-feed");
 
   const template = `
-  <header>
-    <nav>
+  <header class="header-menu">
       <div class="menu">
         <img src="./image/home.png" alt="icone de home" href="#" />
         <img src="./image/publicar.png" src="ic_outline-add-circle-outline.png" alt="icone para publicar um post" href="#"/>
         <img src="./image/sair.png" alt="icone para sair do app" />
       </div>
-    </nav>
   </header>
-  <main>
+  <form class="form-pots"
     <div class="post">
-      <p class="username" id="username"></p>
-      <textarea class="textarea" placeholder="Qual a sua dica/dúvida sobre pele ou cabelo?"></textarea>
+      <div class="text">
+        <p class="username" id="username"></p>
+        <textarea class="textarea" placeholder="Qual a sua dica/dúvida sobre pele ou cabelo?"></textarea>
+      </div>
       <div class="post-button">
         <button type="button" id="button-publish" class="button-publish">Publicar</button>
       </div>
     </div>
-  </main>
+  </form
     `;
 
   container.innerHTML = template;
