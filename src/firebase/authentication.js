40a87cae30ed6cbaef759f5f9498d312d7572d4b
marinacezario <marina.cezario@gmail.com>
2023-04-10 18:00:28 -0300

    teste login Google

--- src/firebase/authentication.js
+++ src/firebase/authentication.js
@@ -12,15 +12,14 @@ import { app } from './configuration.js';
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+
 const loginToFeed = (email, password) => signInWithEmailAndPassword(auth, email, password);
 
 function loginWithGoogle() {
-  return signInWithPopup(auth, provider);
+  const providerGoogle = new GoogleAuthProvider();
+  const authGoogle = getAuth(app);
+  return signInWithPopup(authGoogle, providerGoogle);
 }
-
-const resetPassword = (email) => sendPasswordResetEmail(auth, email);
-
-const logout = () => signOut(auth, provider);
 const register = async (name, email, password) => {
   const authenticateRegister = getAuth(app);
   await createUserWithEmailAndPassword(authenticateRegister, email, password);
@@ -30,8 +29,11 @@ const register = async (name, email, password) => {
   });
 };
 
+const resetPassword = (email) => sendPasswordResetEmail(auth, email);
+
+const logout = () => signOut(auth, provider);
+
 export {
-  auth,
   loginToFeed,
   loginWithGoogle,
   logout,
