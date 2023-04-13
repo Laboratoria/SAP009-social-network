967d5f27b838986cf6e5a78960dc469d66053033
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-04-13 16:19:14 -0300

    criação de post ok

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,12 +1,40 @@
-import { collection, addDoc } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  // onSnapshot,
+  // orderBy,
+  // updateDoc,
+  // arrayUnion,
+  // arrayRemove,
+  // doc,
+  // query,
+  // getDocs,
+  // deleteDoc,
+} from 'firebase/firestore';
 
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
+import { app } from './configuration.js';
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+export const userData = (name, username) => addDoc(collection(db, 'users'), {
+  name,
+  username,
+});
+
+export async function newPost(textpost) {
+  const createPosts = {
+    userId: auth.currentUser.uid,
+    username: auth.currentUser.displayName,
+    date: new Date(),
+    post: textpost,
+    likes: 0,
+  };
+  const docRef = await addDoc(collection(db, 'posts'), createPosts);
+  createPosts.id = docRef.id;
+  console.log(createPosts);
+  return createPosts;
 }
