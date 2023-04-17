e739efe259d83eca4a6db23fe031b148a05d7207
tamiresns <tamiresmn@live.com>
2023-04-17 15:26:46 -0300

    comecando o firebasestore

--- src/pages/firebase/firebasestore.js
+++ src/pages/firebase/firebasestore.js
@@ -1,4 +1,13 @@
-import { getFirestore } from "firebase/firestore";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { app } from "firebase.js";
+import { async } from "regenerator-runtime";
 
 const db = getFirestore(app);
+
+export const newPost = async(date, id, like, text, username) => addDoc(collection(db, 'Post'),{
+    username,
+    date,
+    post,
+    id,
+    likes: 0
+});
