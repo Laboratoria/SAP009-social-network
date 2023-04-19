1d818f88e286e275be6f0b56a4c9d927d4b77bdb
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-04-19 16:26:56 -0300

    timeline css

--- src/js/pages/feed/feed.js
+++ src/js/pages/feed/feed.js
@@ -15,8 +15,10 @@ export default () => {
   <div class="write-post">
     <h2>Share your thoughts...</h2>
     <div class="user-post-area">
+    <div class="user-area">  
       <img src="img/user-icon.png" class="user-icon">
       <div class="display-username"></div>
+    </div>
       <div class="post-area">
         <div class="textarea-div">
           <textarea placeholder="What's up?" class="text-area" required></textarea>
