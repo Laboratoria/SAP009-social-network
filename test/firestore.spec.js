7549a7eba31d0340b527c472085f8b7b697290df
vanessa <vanessa_menezes_18@hotmail.com>
2023-04-20 16:12:25 -0300

    atualizando testes

--- test/firestore.spec.js
+++ test/firestore.spec.js
@@ -1,24 +1,65 @@
 import {
+  auth,
   addDoc,
   collection,
   doc,
+  getDoc,
+  getDocs,
+  query,
+  orderBy,
   updateDoc,
   arrayUnion,
+  arrayRemove,
   deleteDoc,
 } from 'firebase/firestore';
 
 import {
   db,
+  getPosts,
   newPost,
+  getPost,
   editPost,
   likePost,
   deletePost,
 } from '../src/firebase/firestore';
 
 jest.mock('firebase/firestore');
+jest.mock('firebase/auth');
 
 beforeEach(() => {
   jest.clearAllMocks();
+});
+
+describe('getPosts', () => {
+  it('must get a list of posts', async () => {
+    const mockPost1 = {
+      id: 'idPost1',
+      text: 'conteudoPost1',
+      publishDate: new Date(),
+    };
+    const mockPost2 = {
+      id: 'idPost2',
+      text: 'conteudoPost2',
+      publishDate: new Date(),
+    };
+    const mockPostCollection = {
+      forEach: (callback) => {
+        callback(mockPost1);
+        callback(mockPost2);
+      },
+    };
+    const mockQuerySnapshot = {
+      data: jest.fn(() => mockPostCollection),
+    };
+    getDocs.mockResolvedValue(mockQuerySnapshot);
+    const expectedArrayPosts = [{ id: 'idPost1', ...mockPost1 }, { id: 'idPost2', ...mockPost2 }];
+    const result = await getPosts();
+    expect(getDocs).toHaveBeenCalledTimes(1);
+    expect(getDocs).toHaveBeenCalledWith(
+      query(collection(db, 'posts'), orderBy('publishDate', 'desc')),
+    );
+    expect(result).toEqual(expectedArrayPosts);
+  });
 });
 
 describe('newPost', () => {
@@ -27,8 +68,8 @@ describe('newPost', () => {
     const mockCollection = 'collection';
     collection.mockReturnValueOnce(mockCollection);
 
-    const postText = 'text'; // Usar o nome correto do parâmetro
-    const posts = { // Usar as propriedades corretas da coleção "posts"
+    const postText = 'text';
+    const posts = {
       userId: 'userIdTest',
       userName: 'userNameTest',
       text: postText,
@@ -40,7 +81,30 @@ describe('newPost', () => {
     expect(addDoc).toHaveBeenCalledTimes(1);
     expect(addDoc).toHaveBeenCalledWith(mockCollection, posts);
     expect(collection).toHaveBeenCalledTimes(1);
-    expect(collection).toHaveBeenCalledWith(db, 'posts'); // Usar o objeto "db" corretamente
+    expect(collection).toHaveBeenCalledWith(db, 'posts');
+  });
+});
+
+describe('getPost', () => {
+  it('must get a post', async () => {
+    const mockPost = {
+      text: 'conteudoPost',
+      publishDate: new Date(),
+    };
+    const mockQuerySnapshot = {
+      data: jest.fn(() => mockPost),
+      id: 'idPost',
+    };
+    getDoc.mockResolvedValue(mockQuerySnapshot);
+    const postId = 'idPost';
+    const expectedPost = {
+      id: 'idPost',
+      ...mockPost,
+    };
+    const result = await getPost(postId);
+    expect(getDoc).toHaveBeenCalledTimes(1);
+    expect(getDoc).toHaveBeenCalledWith(doc(db, 'posts', postId));
+    expect(result).toEqual(expectedPost);
   });
 });
 
@@ -50,13 +114,13 @@ describe('editPost', () => {
     const mockDoc = 'doc';
     doc.mockReturnValueOnce(mockDoc);
     const postId = 'idPost';
-    const textEdit = 'conteudoPost'; // Usar o nome correto do parâmetro
+    const textEdit = 'conteudoPost';
     const updatedPost = {
-      text: textEdit, // Usar a propriedade correta a ser atualizada
+      text: textEdit,
     };
     await editPost(postId, textEdit);
     expect(doc).toHaveBeenCalledTimes(1);
-    expect(doc).toHaveBeenCalledWith(db, 'posts', postId); // Usar o objeto "db" corretamente
+    expect(doc).toHaveBeenCalledWith(db, 'posts', postId);
     expect(updateDoc).toHaveBeenCalledTimes(1);
     expect(updateDoc).toHaveBeenCalledWith(mockDoc, updatedPost);
   });
@@ -64,23 +128,80 @@ describe('editPost', () => {
 
 describe('likePost', () => {
   it('must add the username to the list of likes', async () => {
+    // criando valores falsos para a função likePost
     updateDoc.mockResolvedValue();
     const mockDoc = 'doc';
     doc.mockReturnValueOnce(mockDoc);
     const mockUnion = 'union';
     arrayUnion.mockReturnValueOnce(mockUnion);
 
-    const postId = 'id-post';
+    // criando valores de entrada:
+    const postId = 'id-post'; // ID de post fictício
     const updatedPost = {
-      likes: mockUnion,
+      like: mockUnion,
     };
     await likePost(postId);
-    expect(doc).toHaveBeenCalledTimes(1);
-    expect(doc).toHaveBeenCalledWith(db, 'posts', postId);
+
+    expect(doc).toHaveBeenCalledTimes(1); // verifica se a função foi chamada exatamente uma vez
+    expect(doc).toHaveBeenCalledWith(db, 'posts', postId); // verifica se foi chamada com os parâmetros esperados.
     expect(updateDoc).toHaveBeenCalledTimes(1);
     expect(updateDoc).toHaveBeenCalledWith(mockDoc, updatedPost);
   });
+
+  it('must remove the username from the list of likes if the user has already liked the post', async () => {
+    // criando valores falsos para a função likePost
+    updateDoc.mockResolvedValue();
+    const mockDoc = 'doc';
+    doc.mockReturnValueOnce(mockDoc);
+    const mockRemove = 'remove';
+    arrayRemove.mockReturnValueOnce(mockRemove);
+
+    // criando valores de entrada:
+    const postId = 'id-post'; // ID de post fictício
+    const post = {
+      like: [auth.currentUser.uid],
+    };
+    const updatedPost = {
+      like: mockRemove,
+    };
+    getPost.mockResolvedValueOnce(post);
+
+    await likePost(postId);
+
+    expect(getPost).toHaveBeenCalledTimes(1); // verifica se a função foi chamada exatamente uma vez
+    expect(getPost).toHaveBeenCalledWith(postId); // verifica se foi chamada os parâmetros esperados
+    expect(doc).toHaveBeenCalledTimes(2); // verifica se a função foi chamada exatamente duas vezes
+    expect(doc).toHaveBeenNthCalledWith(1, db, 'posts', postId); // verifica se foi chamada com os parâmetros esperados.
+    expect(doc).toHaveBeenNthCalledWith(2, db, 'posts', postId); // verifica se foi chamada com os parâmetros esperados.
+    expect(updateDoc).toHaveBeenCalledTimes(1);
+    expect(updateDoc).toHaveBeenCalledWith(mockDoc, updatedPost);
+
+    const updatedLikeList = await likePost(postId);
+    expect(updatedLikeList).toEqual([]);
+  });
 });
+
+// describe('likePost', () => {
+//   it('must add the username to the list of likes', async () => {
+//     updateDoc.mockResolvedValue(); // criando valores falsos para a função likePost
+//     const mockDoc = 'doc';
+//     doc.mockReturnValueOnce(mockDoc);
+//     const mockUnion = 'union';
+//     arrayUnion.mockReturnValueOnce(mockUnion);
+
+//     // criando valores de entrada:
+//     const postId = 'id-post'; // ID de post fictício
+//     const updatedPost = {
+//       like: mockUnion,
+//     };
+//     await likePost(postId);
+//     expect(doc).toHaveBeenCalledTimes(1); // verifica se a função foi chamada exatamente uma vez
+// eslint-disable-next-line max-len
+//     expect(doc).toHaveBeenCalledWith(db, 'posts', postId); // verifica se foi chamada com os parâmetros esperados.
+//     expect(updateDoc).toHaveBeenCalledTimes(1);
+//     expect(updateDoc).toHaveBeenCalledWith(mockDoc, updatedPost);
+//   });
+// });
 
 describe('deletePost', () => {
   it('must delete the specified post', async () => {
