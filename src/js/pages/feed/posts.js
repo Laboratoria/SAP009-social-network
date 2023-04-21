917605191dee7a829c80d411395943af9f95568b
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-04-21 15:46:09 -0300

    tudo certo, nada funcionando

--- src/js/pages/feed/posts.js
+++ src/js/pages/feed/posts.js
@@ -8,7 +8,7 @@ import {
 export function postTemplate(post) {
   const postContainer = document.createElement('section');
   postContainer.classList.add('post-section');
-  const countLikes = post.likes;
+  let countLikes = post.likes.length;
   const userData = getUserData();
   const isAuthor = userData.uid === post.userId;
 
@@ -26,19 +26,6 @@ export function postTemplate(post) {
     return '';
   };
 
-  const likePost = () => {
-    if (!isAuthor) {
-      return `
-      <div class="number-like">${countLikes.length}</div>
-      <button type="button" class="like-btn footer-btn">
-        <img src="img/heart-regular.png" class="not-liked post-img">
-        <img src="img/heart-solid.png" class="liked" post-img>
-      </button>
-    `;
-    }
-    return '';
-  };
-
   const template = `
   <div class="post-list">
    <hr> 
@@ -50,73 +37,84 @@ export function postTemplate(post) {
    <footer class="footer-post">
      <div class="post-date">${post.date.toDate().toLocaleDateString('pt-BR')}</div>
      <div class="post-edit-delete edit-delete">${editUserPost()}</div>
-     <div class="post-like">${likePost()}</div>
+     <div class="post-like">
+      <div class="number-like">${countLikes}</div>
+      <button type="button" class="like-btn footer-btn">
+       <img src="img/heart-regular.png" class="not-liked post-img">
+       <img src="img/heart-solid.png" class="liked post-img">
+      </button>
+     </div>
    </footer>
   </div>
   `;
   postContainer.innerHTML = template;
 
-  // const postLike = postContainer.querySelector('.post-like');
-  const likeButton = postContainer.querySelector(`#like-button-${post.id}`);
-  const disliked = postContainer.querySelector(`#not-liked-${post.id}`);
-  const liked = postContainer.querySelector(`#liked-${post.id}`);
-  // const likesCounter = postLike.querySelector('#number-like');
+  const likeButton = postContainer.querySelector('.like-btn');
+  const disliked = postContainer.querySelector('.not-liked');
+  const liked = postContainer.querySelector('.liked');
+  const likesCounter = postContainer.querySelector('.number-like');
+  const likesCollection = post.likes;
 
-  if (likeButton) {
-    if (countLikes.includes(userData.uid)) {
+  if (likesCollection.includes(userData.uid)) {
+    disliked.style.display = 'none';
+    liked.style.display = 'flex';
+  }
+
+  likeButton.addEventListener('click', () => {
+    if (likesCollection.includes(userData.uid)) {
+      dislikePosts(post.id, userData.uid);
+      disliked.style.display = 'flex';
+      liked.style.display = 'none';
+      countLikes -= 1;
+      likesCounter.innerHTML = countLikes;
+    } else {
+      likePosts(post.id, userData.uid);
       disliked.style.display = 'none';
       liked.style.display = 'flex';
+      countLikes += 1;
+      likesCounter.innerHTML = countLikes;
+      likesCollection.push(userData.uid);
     }
-
-    likeButton.addEventListener('click', () => {
-      if (countLikes.includes(userData.uid)) {
-        dislikePosts(post.id, userData.uid);
-        disliked.style.display = 'flex';
-        liked.style.display = 'none';
-        // likesCounter.innerHTML = countLikes.length;
-      } else {
-        likePosts(post.id, userData.uid);
-        disliked.style.display = 'none';
-        liked.style.display = 'flex';
-        // likesCounter.innerHTML = countLikes.length;
-      }
-    });
-  }
+  });
 
   const bodyPost = postContainer.querySelector(`#body-post-${post.id}`);
   const editDeletePost = postContainer.querySelector('.post-edit-delete');
   const editBtn = postContainer.querySelector('.edit-btn');
 
-  const saveCancelBtn = `
-    <div class="post-editing edit-delete" id="post-editing-${post.id}">
+  const saveCancelBtn = () => {
+    if (editBtn) {
+      return `
+    <div class="post-editing" id="post-editing-${post.id}">
       <button type="button" class="save-btn editing-buttons">SAVE</button>
       <button type="button" class="cancel-btn editing-buttons">CANCEL</button>
     </div>
   `;
+    }
+    return '';
+  };
 
   const saveBtn = postContainer.querySelector('.save-btn');
   const cancelBtn = postContainer.querySelector('.cancel-btn');
-  console.log(editBtn);
   if (editBtn) {
     editBtn.addEventListener('click', () => {
       bodyPost.removeAttribute('disabled');
       editDeletePost.innerHTML = '';
-      editDeletePost.innerHTML = saveCancelBtn;
+      editDeletePost.innerHTML = saveCancelBtn();
     });
-
-    if (saveBtn) {
-      saveBtn.addEventListener('click', () => {
-        editPost(post.id, bodyPost.value);
-        bodyPost.setAttribute('disabled');
-      });
-    }
-
-    if (cancelBtn) {
-      cancelBtn.addEventListener('click', () => {
-        bodyPost.setAttribute('disabled');
-        bodyPost.innerHTML = `${post.post}`;
-      });
-    }
+  }
+  console.log(cancelBtn);
+  if (cancelBtn) {
+    cancelBtn.addEventListener('click', () => {
+      console.log('clicked');
+      bodyPost.setAttribute('disabled');
+      bodyPost.innerHTML = `${post.post}`;
+    });
+  }
+  if (saveBtn) {
+    saveBtn.addEventListener('click', () => {
+      editPost(post.id, bodyPost.value);
+      bodyPost.setAttribute('disabled');
+    });
   }
   return postContainer;
 }
