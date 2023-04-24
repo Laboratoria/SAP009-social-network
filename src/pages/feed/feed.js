a7ea23fde7363980de0757ea358961787576c943
Edimara <edimaraarcanjo21@gmail.com>
2023-04-24 10:59:41 -0300

    deletar, css e bts do feed ok

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -56,14 +56,16 @@ export default () => {
                   <p class="user-name">${posts.username}</p>
                   <p class ="dataPost">${posts.date}</p>
                   </div>
-                  <textarea name="" id="txt-area-postado" cols="70" rows="5">${posts.text}</textarea>
+                  <textarea disabled name="" id="txt-area-postado" cols="70" rows="5">${posts.text}</textarea>
                   <div class="position-btn-postar">
+                  ${posts.userId === auth.currentUser.uid ? `  
                   <button id="${posts.id}editar" class="btn-postar editado">
                   <img class='editar-img' src='./img/editar-informacao.png' alt='logo-google'>
                     </button>
                     <button id="${posts.id}deletar" class="btn-postar delete">
                       <img class='excluir-img' src='./img/botao-apagar.png' alt='logo-google'>
-                      </button>
+                      </button>` : ''}
+                
                       <button id="${posts.id}like" class="btn-postar like">
                      <img class='curtir-img' src='./img/ame.png' alt='logo-google'>
                      <label id="likes-quantities">${posts.like}</label>
@@ -76,44 +78,41 @@ export default () => {
       </section>     
     `).join('');
 
-        container.querySelector('.feed-postado').innerHTML = postList;
+    container.querySelector('.feed-postado').innerHTML = postList;
 
-        arrayPosts.forEach(post => {
-            const btnDeletar = document.getElementById(post.id + 'deletar');
-            btnDeletar.addEventListener('click', (e) => {
-                e.preventDefault();
-                if (window.confirm('Tem certeza de que deseja excluir a publicação?')) {
-                    deletarPost(post.id)
-                        .then(() => {
-                            const areaPostado = document.getElementById(post.id);
-                            areaPostado.remove();
-                        });
-                };
+    arrayPosts.forEach(post => {
+      const btnDeletar = document.getElementById(post.id + 'deletar');
+      btnDeletar.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (window.confirm('Tem certeza de que deseja excluir a publicação?')) {
+          deletarPost(post.id)
+            .then(() => {
+              const areaPostado = document.getElementById(post.id);
+              areaPostado.remove();
             });
-        });
-    }
-    printPost();
-
-    const textArea = container.querySelector('#txt-area');
-    const btnPublicar = container.querySelector('#bntPublicar');
-    btnPublicar.addEventListener('click', () => {
-        if (textArea.value !== '') {
-            const today = new Date();
-            const userName = auth.currentUser.displayName;
-            const idUser = auth.currentUser.uid;
-
-            salvarPost(today, idUser, textArea.value, userName).then(() => {
-                printPost();
-            });
-        } else { alert('Por favor, preencha o campo de postagem!'); }
+        };
+      });
     });
+  }
+  printPost();
 
+  const textArea = container.querySelector('#txt-area');
+  const btnPublicar = container.querySelector('#bntPublicar');
+  btnPublicar.addEventListener('click', () => {
+    if (textArea.value !== '') {
+      const today = new Date();
+      const userName = auth.currentUser.displayName;
+      const idUser = auth.currentUser.uid;
 
-    const btnSair = container.querySelector('.btn-sair');
-    btnSair.addEventListener('click', () => {
-        window.location.hash = '#login';
-    });
-    return container;
+      salvarPost(today, idUser, textArea.value, userName).then(() => {
+        printPost();
+      });
+    } else { alert('Por favor, preencha o campo de postagem!'); }
+  });
+
+  const btnSair = container.querySelector('.btn-sair');
+  btnSair.addEventListener('click', () => {
+    window.location.hash = '#login';
+  });
+  return container;
 };
-
-
