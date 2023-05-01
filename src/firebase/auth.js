234cbc3bc42ba98ed9f9be50e271e1b652f8055c
michelefeitosa <michelefeitosa08@gmail.com>
2023-04-30 22:10:12 -0300

    adicionando funções do firebase

---
+++ src/firebase/auth.js
@@ -0,0 +1,37 @@
+import { getAuth, createUserWithEmailAndPassword,signOut } from "firebase/auth";
+//criar conta 
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  //fazer login 
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+  //desconectar 
+
+const auth = getAuth();
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
