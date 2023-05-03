87d161c74b65bda7be718d0f443d1111d3cddecb
raianefsantos <raianefsantos13@gmail.com>
2023-05-03 16:26:32 -0300

    css

--- src/main.js
+++ src/main.js
@@ -1,29 +1,31 @@
-import { Home } from './pages/home/home.js';
-import { Cadastro } from './pages/Sign up/cadastro.js';
-import { Feed } from './pages/Post/post.js';
 
-const main = document.getElementById('root');
+import { Home } from './pages/home/home.js';
+import { Cadastro } from './pages/cadastro/cadastro.js';
+import { Feed } from './pages/feed/feed.js';
+
+const main = document.getElementById('root')
 const init = () => {
-  window.addEventListener("hashchange", () => {
-    console.log(window.location.hash);
-    main.innerHTML = ""; //impede que a página se repita
-    switch(window.location.hash){
-      case "#":
-        main.appendChild(Home());
-        break;
-      case "#cadastro":
-        main.appendChild(Cadastro());
-        break;
-      case "#feed":
-        main.appendChild(Feed());
-        break;
-      default:
-        main.appendChild(Home()); 
-    }
-  })
+window.addEventListener("hashchange", () => {
+console.log(window.location.hash);
+main.innerHTML = ""; //impede que a página se repita
+switch(window.location.hash){
+case "#":
+main.appendChild(Home());
+break;
+case "#cadastro":
+main.appendChild(Cadastro());
+break;
+case "#feed":
+main.appendChild(Feed());
+break;
+default:
+main.appendChild(Home());
+}
+})
 }
 
 window.addEventListener("load", () => {
-  main.appendChild(Home());
-  init();
+main.appendChild(Home());
+init();
 })
+
