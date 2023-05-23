947f7b5a1bf2d58506f5097ab342b4f0be3687a3
julzita <juliaellen1996@gmail.com>
2023-05-23 13:59:40 -0300

    testes completos

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,8 @@
 // importamos la funcion que vamos a testear
 import { signIn, createUser, logOut } from '../src/firebase/auth.js';
+import {
+  addPost, deletePost, editPost, printPost,
+} from '../src/firebase/config.js';
 
 it('should execute firebase login function', () => {
   const email = 'email@gmail.com';
@@ -21,4 +24,31 @@ it('should execute firebase logout function', () => {
   const password = 'password';
   logOut(email, password);
   expect(typeof logOut).toBe('function');
+});
+
+it('should execute firebase post function', () => {
+  const date = '';
+  const post = '';
+  const username = '';
+  addPost(date, post, username);
+  expect(typeof addPost).toBe('function');
+});
+
+it('should execute firebase delete post function', () => {
+  const postId = 'posts';
+  deletePost(postId);
+  expect(typeof deletePost).toBe('function');
+});
+
+it('should execute firebase edit post function', () => {
+  const postId = 'posts';
+  const newPostData = 'post: newPostData';
+  editPost(postId, newPostData);
+  expect(typeof editPost).toBe('function');
+});
+
+it('should execute firebase print post function', () => {
+  const posts = 'posts';
+  printPost(posts);
+  expect(typeof printPost).toBe('function');
 });
